f99a63c59dcf0b81124ca42730c97638c102e798
colinhiggs <colin.higgs@ed.ac.uk>
2015-07-29 22:35:57 +0100

    reorganise and comment __iniit__.py

--- test_project/test_project/__init__.py
+++ test_project/test_project/__init__.py
@@ -1,27 +1,33 @@
 from pyramid.config import Configurator
 from sqlalchemy import engine_from_config
 
+# The jsonapi module.
 import jsonapi
+# Import models as a module: needed for create_jsonapi...
 from . import models
-
-#from .models import (
-#    DBSession,
-#    Base,
-#    )
-
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    # The usual stuff from the pyramid alchemy scaffold.
     engine = engine_from_config(settings, 'sqlalchemy.')
     jsonapi.DBSession.configure(bind=engine)
     models.Base.metadata.bind = engine
     config = Configurator(settings=settings)
-    config.include('cornice')
-    renderer = jsonapi.JSONAPIFromSqlAlchemyRenderer()
-    config.add_renderer('jsonapi', renderer)
-    jsonapi.create_jsonapi_using_magic_and_pixie_dust(models)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
+
+    # jsonapi requires cornice.
+    config.include('cornice')
+
+    # Lines specific to jsonapi.
+    # Set up the renderer.
+    renderer = jsonapi.JSONAPIFromSqlAlchemyRenderer()
+    config.add_renderer('jsonapi', renderer)
+    # Create the routes and views automagically.
+    jsonapi.create_jsonapi_using_magic_and_pixie_dust(models)
+    # Make sure we scan the *jsonapi* package.
     config.scan(package=jsonapi)
+
+    # Back to the usual pyramid stuff.
     return config.make_wsgi_app()
