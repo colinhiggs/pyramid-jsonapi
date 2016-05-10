ad74ef24556990d5d2c1f02f123b8b874b41d552
colinhiggs <colin.higgs@ed.ac.uk>
2016-05-10 10:05:30 +0100

    add tests aligned to spec document (http://jsonapi.org/format)

---
+++ test_project/test_project/test_spec.py
@@ -0,0 +1,268 @@
+import unittest
+import transaction
+import testing.postgresql
+import webtest
+from pyramid.paster import get_app
+from sqlalchemy import create_engine
+
+#from pyramid import testing
+
+from .models import (
+    DBSession,
+    Base,
+    Person,
+    Blog,
+    Post
+)
+
+from . import test_data
+from .test_data import (
+    data,
+    idx,
+    npeople, nblogs, nposts,
+    nblogs_per_person, nposts_per_blog
+)
+
+class TestSpec(unittest.TestCase):
+    '''Test compliance against jsonapi spec.
+
+    http://jsonapi.org/format/
+    '''
+
+    @classmethod
+    def setUpClass(cls):
+        '''Create a test DB and import data.'''
+        # Create a new database somewhere in /tmp
+        cls.postgresql = testing.postgresql.Postgresql(port=7654)
+        cls.engine = create_engine(cls.postgresql.url())
+        DBSession.configure(bind=cls.engine)
+
+        cls.app = get_app('testing.ini')
+        cls.test_app = webtest.TestApp(cls.app)
+
+    @classmethod
+    def tearDownClass(cls):
+        '''Throw away test DB.'''
+        DBSession.close()
+        cls.postgresql.stop()
+
+    def setUp(self):
+        Base.metadata.create_all(self.engine)
+        # Add some basic test data.
+        test_data.add_to_db()
+        transaction.begin()
+
+    def tearDown(self):
+        transaction.abort()
+        Base.metadata.drop_all(self.engine)
+
+    def test_spec_server_content_type(self):
+        '''Response should have correct content type.
+
+        Servers MUST send all JSON API data in response documents with the
+        header Content-Type: application/vnd.api+json without any media type
+        parameters.
+        '''
+        # Fetch a representative page
+        r = self.test_app.get('/people')
+        self.assertEqual(r.content_type, 'application/vnd.api+json')
+
+    def test_spec_incorrect_client_content_type(self):
+        '''Server should return error if we send media type parameters.
+
+        Servers MUST respond with a 415 Unsupported Media Type status code if a
+        request specifies the header Content-Type: application/vnd.api+json with
+        any media type parameters.
+        '''
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Content-Type': 'application/vnd.api+json; param=val' },
+            status=415,
+        )
+
+    def test_spec_accept_not_acceptable(self):
+        '''Server should respond with 406 if all jsonapi media types have parameters.
+
+        Servers MUST respond with a 406 Not Acceptable status code if a
+        request’s Accept header contains the JSON API media type and all
+        instances of that media type are modified with media type parameters.
+        '''
+        # Should work with correct accepts header.
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Accept': 'application/vnd.api+json' },
+        )
+        # 406 with one incorrect type.
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Accept': 'application/vnd.api+json; param=val' },
+            status=406,
+        )
+        # 406 with more than one type but none without params.
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Accept': 'application/vnd.api+json; param=val,' +
+                'application/vnd.api+json; param2=val2' },
+            status=406,
+        )
+
+    def test_spec_toplevel_must(self):
+        '''Server response must have one of data, errors or meta.
+
+        A JSON object MUST be at the root of every JSON API request and response
+        containing data.
+
+        A document MUST contain at least one of the following top-level members:
+
+            * data: the document’s “primary data”
+            * errors: an array of error objects
+            * meta: a meta object that contains non-standard meta-information.
+        '''
+        # Should be object with data member.
+        r = self.test_app.get('/people')
+        self.assertIn('data', r.json)
+        # Should also have a meta member.
+        self.assertIn('meta', r.json)
+
+        # Should be object with an array of errors.
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Content-Type': 'application/vnd.api+json; param=val' },
+            status=415,
+        )
+        self.assertIn('errors', r.json)
+        self.assertIsInstance(r.json['errors'], list)
+
+    def test_spec_get_primary_data_empty(self):
+        '''Should return an empty list of results.
+
+        Primary data MUST be either:
+
+            * ...or an empty array ([])
+
+        A logical collection of resources MUST be represented as an array, even
+        if it... is empty.
+        '''
+        r = self.test_app.get('/people?filter[name:eq]=doesnotexist')
+        self.assertEqual(len(r.json['data']), 0)
+
+    def test_spec_get_primary_data_array(self):
+        '''Should return an array of resource objects.
+
+        Primary data MUST be either:
+
+            * an array of resource objects, an array of resource identifier
+            objects, or an empty array ([]), for requests that target resource
+            collections
+        '''
+        # Data should be an array of person resource objects or identifiers.
+        r = self.test_app.get('/people')
+        self.assertIn('data', r.json)
+        self.assertIsInstance(r.json['data'], list)
+        item = r.json['data'][0]
+
+
+    def test_spec_get_primary_data_array_of_one(self):
+        '''Should return an array of one resource object.
+
+        A logical collection of resources MUST be represented as an array, even
+        if it only contains one item...
+        '''
+        r = self.test_app.get('/people?page[limit]=1')
+        self.assertIn('data', r.json)
+        self.assertIsInstance(r.json['data'], list)
+        self.assertEqual(len(r.json['data']), 1)
+
+    def test_spec_get_primary_data_single(self):
+        '''Should return a single resource object.
+
+        Primary data MUST be either:
+
+            * a single resource object, a single resource identifier object, or
+            null, for requests that target single resources
+        '''
+        # Find the id of alice.
+        r = self.test_app.get('/people?filter[name:eq]=alice')
+        item = r.json['data'][0]
+        self.assertEqual(item['attributes']['name'], 'alice')
+        alice_id = item['id']
+        # Now get alice object.
+        r = self.test_app.get('/people/' + alice_id)
+        alice = r.json['data']
+        self.assertEqual(alice['attributes']['name'], 'alice')
+
+    def test_spec_resource_object_must(self):
+        '''Resource object should have at least id and type.
+
+        A resource object MUST contain at least the following top-level members:
+            * id
+            * type
+
+        The values of the id and type members MUST be strings.
+        '''
+        r = self.test_app.get('/people?page[limit]=1')
+        item = r.json['data'][0]
+        # item must have at least a type and id.
+        self.assertEqual(item['type'], 'people')
+        self.assertIn('id', item)
+        self.assertIsInstance(item['type'], str)
+        self.assertIsInstance(item['id'], str)
+
+    def test_spec_resource_object_must(self):
+        '''Fetched resource should have attributes, relationships, links, meta.
+
+        a resource object MAY contain any of these top-level members:
+
+            * attributes: an attributes object representing some of the
+              resource’s data.
+
+            * relationships: a relationships object describing relationships
+              between the resource and other JSON API resources.
+
+            * links: a links object containing links related to the resource.
+
+            * meta: a meta object containing non-standard meta-information about
+              a resource that can not be represented as an attribute or
+              relationship.
+        '''
+        r = self.test_app.get('/people?page[limit]=1')
+        item = r.json['data'][0]
+        self.assertIn('attributes', item)
+        #self.assertIn('relationships', item)
+        self.assertIn('links', item)
+        #self.assertIn('meta', item)
+
+    def test_spec_type_id_identify_resource(self):
+        '''Using type and id should fetch a single resource.
+
+        Within a given API, each resource object’s type and id pair MUST
+        identify a single, unique resource.
+        '''
+        # Find the id of alice.
+        r = self.test_app.get('/people?filter[name:eq]=alice')
+        item = r.json['data'][0]
+        self.assertEqual(item['attributes']['name'], 'alice')
+        alice_id = item['id']
+
+        # Search for alice by id. We should get one result whose name is alice.
+        r = self.test_app.get('/people?filter[id:eq]={}'.format(alice_id))
+        self.assertEqual(len(r.json['data']), 1)
+        item = r.json['data'][0]
+        self.assertEqual(item['id'], alice_id)
+        self.assertEqual(item['attributes']['name'], 'alice')
+
+
+
+    def test_api_errors_structure(self):
+        '''Errors should be array of objects with code, title, detail members.'''
+        r = self.test_app.get(
+            '/people',
+            headers={ 'Content-Type': 'application/vnd.api+json; param=val' },
+            status=415,
+        )
+        self.assertIn('errors', r.json)
+        self.assertIsInstance(r.json['errors'], list)
+        err = r.json['errors'][0]
+        self.assertIn('code', err)
+        self.assertIn('title', err)
+        self.assertIn('detail', err)
