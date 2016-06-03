1730cddc81a6d4efa24961c9ee052c3da27aa4fb
colinhiggs <colin.higgs@ed.ac.uk>
2016-06-03 12:24:59 +0100

    update to deal with loss of pyvenv

--- create-venv.sh
+++ create-venv.sh
@@ -2,12 +2,12 @@
 
 if grep '^Ubuntu 14.04' /etc/issue
 then
-  pyvenv-3.4 --without-pip env
+  python3 -m venv --without-pip env
   source env/bin/activate
-  curl https://bootstrap.pypa.io/get-pip.py | python3.4
+  curl https://bootstrap.pypa.io/get-pip.py | python3
   deactivate
 else
-  pyvenv-3.4 env
+  python3 -m venv env
 fi
 source env/bin/activate
 pip install -r requirements.txt
