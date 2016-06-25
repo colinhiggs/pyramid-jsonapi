25407dc57ddd313568b91a6c4fbd0c6185cf89a0
colinhiggs <colin.higgs@ed.ac.uk>
2016-06-25 20:18:39 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+version = '0.1'
+
+requires = [
+    'pyramid',
+    'SQLAlchemy',
+    ]
+
+setup(
+  name = 'jsonapi',
+  packages = ['jsonapi'],
+  version = version,
+  description = 'Auto-build JSON API from sqlalchemy models using the pyramid framework',
+  author = 'Colin Higgs',
+  author_email = 'colin.higgs70@gmail.com',
+  url = 'https://github.com/colinhiggs/pyramid-jsonapi',
+  download_url =\
+    'https://github.com/colinhiggs/pyramid-jsonapi/tarball/{}'.format(version),
+  keywords = ['json', 'api', 'API', 'JSON-API', 'pyramid', 'sqlalchemy'],
+  classifiers = [],
+)
