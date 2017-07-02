d2b4fd50c8649859ab3adb6ab699ee7ff81d04c2
Colin Higgs <colin.higgs@ed.ac.uk>
2017-07-02 23:06:15 +0100

    Implement extensible filter operator set

--- pyramid_jsonapi/__init__.py
+++ pyramid_jsonapi/__init__.py
@@ -32,7 +32,7 @@ import types
 import importlib
 import itertools
 import logging
-from collections import deque, Sequence
+from collections import deque, Sequence, Mapping
 
 from sqlalchemy.orm import load_only
 from sqlalchemy.exc import DBAPIError
@@ -141,6 +141,10 @@ class PyramidJSONAPI():
         self.models = models
         self.get_dbsession = get_dbsession
         self.endpoint_data = EndpointData(config)
+        self.filter_operator_collection = FilterOperatorCollection()
+        self.filter_operator_collection.register_group(
+            filter_operator_groups['standard']
+        )
 
     @staticmethod
     def error(e, request):
@@ -335,6 +339,7 @@ class PyramidJSONAPI():
         CollectionView.relationships = rels
         fields.update(rels)
         CollectionView.fields = fields
+        CollectionView.filter_operator_collection = self.filter_operator_collection
 
         # All callbacks have the current view as the first argument. The comments
         # below detail subsequent args.
@@ -1776,32 +1781,16 @@ class CollectionViewBase:
             if isinstance(prop.property, RelationshipProperty):
                 # TODO(Colin): deal with relationships properly.
                 pass
-            if op == 'eq':
-                op_func = getattr(prop, '__eq__')
-            elif op == 'ne':
-                op_func = getattr(prop, '__ne__')
-            elif op == 'startswith':
-                op_func = getattr(prop, 'startswith')
-            elif op == 'endswith':
-                op_func = getattr(prop, 'endswith')
-            elif op == 'contains':
-                op_func = getattr(prop, 'contains')
-            elif op == 'lt':
-                op_func = getattr(prop, '__lt__')
-            elif op == 'gt':
-                op_func = getattr(prop, '__gt__')
-            elif op == 'le':
-                op_func = getattr(prop, '__le__')
-            elif op == 'ge':
-                op_func = getattr(prop, '__ge__')
-            elif op == 'like' or op == 'ilike':
-                op_func = getattr(prop, op)
-                val = re.sub(r'\*', '%', val)
-            else:
+            try:
+                filter_op = self.filter_operator_collection[op]
+            except KeyError:
                 raise HTTPBadRequest(
                     "No such filter operator: '{}'".format(op)
                 )
-            q = q.filter(op_func(val))
+            if filter_op.value_transform:
+                val = filter_op.value_transform(val)
+            db_func = filter_op.get_db_function(prop)
+            q = q.filter(db_func(val))
 
         return q
 
@@ -2462,6 +2451,111 @@ class CollectionViewBase:
         '''
         for cb_name, callback in callback_sets[set_name].items():
             cls.callbacks[cb_name].append(callback)
+
+
+class FilterOperator:
+    '''Encapsulates an operator for use in filter parameter.
+
+    Arguments:
+        name (str): name used in filter parameter of the URL
+
+    Keyword Args:
+        get_db_function (function): returns a function implementing database op.
+        Will be given the property (attribute or relationship) as the first
+        argument.
+
+    Attributes:
+        name (str): name
+
+        get_db_function (function):
+    '''
+
+    def __init__(
+        self,
+        name,
+        get_db_function=None,
+        value_transform=None,
+        docstring=None
+    ):
+        self.name = name
+        self.get_db_function = get_db_function
+        self.value_transform = value_transform
+        self.docstring = docstring
+
+
+class FilterOperatorCollection(Mapping):
+    '''Contains FilterOperator instances.'''
+
+    def __init__(self):
+        self.ops = {}
+
+    def register(self, operator, force=False):
+        self.check_interface_filter_operator(operator)
+        if operator.name in self.ops and not force:
+            raise KeyError('Operator named "{}" already registered'.format(
+                operator.name
+            ))
+        self.ops[operator.name] = operator
+
+    def register_group(self, iterable):
+        for op in iterable:
+            self.register(op)
+
+    def check_interface_filter_operator(self, operator):
+        '''Check operator for FilterOperator interface.'''
+        for att in ('name', 'get_db_function', 'value_transform', 'docstring'):
+            if not hasattr(operator, att):
+                raise Exception('operator must have attribute {}'.format(att))
+
+    def __getitem__(self, key):
+        return self.ops[key]
+
+    def __iter__(self):
+        return iter(self.ops)
+
+    def __len__(self):
+        return len(self.ops)
+
+
+filter_operator_groups = {
+    'standard': [
+        FilterOperator(
+            'eq', lambda prop: getattr(prop, '__eq__')
+        ),
+        FilterOperator(
+            'ne', lambda prop: getattr(prop, '__ne__'),
+        ),
+        FilterOperator(
+            'startswith', lambda prop: getattr(prop, 'startswith'),
+        ),
+        FilterOperator(
+            'endswith', lambda prop: getattr(prop, 'endswith'),
+        ),
+        FilterOperator(
+            'contains', lambda prop: getattr(prop, 'contains'),
+        ),
+        FilterOperator(
+            'lt', lambda prop: getattr(prop, '__lt__'),
+        ),
+        FilterOperator(
+            'gt', lambda prop: getattr(prop, '__gt__'),
+        ),
+        FilterOperator(
+            'le', lambda prop: getattr(prop, '__le__'),
+        ),
+        FilterOperator(
+            'ge', lambda prop: getattr(prop, '__ge__'),
+        ),
+        FilterOperator(
+            'like', lambda prop: getattr(prop, 'like'),
+            value_transform=lambda val: re.sub(r'\*', '%', val)
+        ),
+        FilterOperator(
+            'ilike', lambda prop: getattr(prop, 'ilike'),
+            value_transform=lambda val: re.sub(r'\*', '%', val)
+        ),
+    ]
+}
 
 
 def acso_after_serialise_object(view, obj):
