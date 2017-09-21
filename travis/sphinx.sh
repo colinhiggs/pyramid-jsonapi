13bd340f60f29ae27b2b88e6f2f1c51bf52debcc
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-09-21 16:09:56 +0100

    -T prevents modules.rst from being created.

--- travis/sphinx.sh
+++ travis/sphinx.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 if [[ $TRAVIS_BRANCH == "master" ]]; then
-  # Generate api docs, rmeove modules.rst as we don't
-  # Use it and it's xistence raises an error
-  sphinx-apidoc -o docs/source pyramid_jsonapi
-  rm -f docs/source/modules.rst
+  sphinx-apidoc -T -o docs/source pyramid_jsonapi
   travis-sphinx build
   travis-sphinx deploy
 fi
