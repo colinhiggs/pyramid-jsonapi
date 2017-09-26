8d7c8e20cf2c0721fa9fc50143c6c9e66b40eaa7
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-09-26 15:31:57 +0100

    Only build sphinx docs for tagged releases on master branch.

--- travis/sphinx.sh
+++ travis/sphinx.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == "master" ]]; then
+if [[ $TRAVIS_BRANCH == "master" && -n $TRAVIS_TAG ]]; then
   sphinx-apidoc -T -e -o docs/source/apidoc pyramid_jsonapi
   # Generate config docs from python method
   python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); s.sphinx_doc()' >docs/source/apidoc/settings.inc
