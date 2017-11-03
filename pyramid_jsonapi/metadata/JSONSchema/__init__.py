262ffe4d9e3872fef14d743d789b1b58187aaf97
Matthew Richardson <m.richardson@ed.ac.uk>
2017-11-03 14:51:08 +0000

    Add endpoint_schema methods. Fix id handling.

--- pyramid_jsonapi/metadata/JSONSchema/__init__.py
+++ pyramid_jsonapi/metadata/JSONSchema/__init__.py
@@ -1,5 +1,6 @@
 """JSONSchema metadata plugin."""
 
+import copy
 import functools
 import json
 import logging
@@ -43,6 +44,12 @@ class JSONSchema():
             VIEWS(
                 attr='resource_attributes_view',
                 route_name='resource/{endpoint}',
+                request_method='',
+                renderer=''
+            ),
+            VIEWS(
+                attr='endpoint_schema_view',
+                route_name='endpoint/{endpoint}{sep:/?}{method:.*}',
                 request_method='',
                 renderer=''
             ),
@@ -120,5 +127,57 @@ class JSONSchema():
             schema.update(factory(view_class.model))
         except alchemyjsonschema.InvalidStatus as exc:
             logging.warning("Schema Error: %s", exc)
+
+
+
+        # Remove 'id' attribute
+        del schema['properties']['id']
+        if 'id' in schema['required']:
+            schema['required'].remove('id')
+
+        return schema
+
+
+    def endpoint_schema_view(self, request):
+        """Pyramid view for endpoint_schema.
+        Parameters:
+            request: Pyramid Request object.
+
+        Returns:
+            Results of endpoint_schema() method call.
+
+        Raises:
+            HTTPNotFound error for unknown endpoints.
+
+        Takes 2 path params: endpoint[/method] (defaults to get).
+        """
+
+        endpoint = request.matchdict['endpoint']
+        method = request.matchdict.get('method')
+        return self.endpoint_schema(endpoint, method)
+
+    def endpoint_schema(self, endpoint, method):
+        """Generate a full schema for an endpoint.
+
+        Parameters:
+            endpoint (string): Endpoint name
+            method (string): http method (defaults to 'get')
+
+        Returns:
+            JSONSchema (dict)
+        """
+
+        if not method:
+            method = 'get'
+        method = method.lower()
+
+        schema = copy.deepcopy(self.schema)
+        if method == 'post':
+            schema['definitions']['resource']['required'].remove('id')
+
+        attrs = self.resource_attributes(endpoint)
+        props = schema['definitions']['resource']['properties']
+        props['attributes'] = attrs
+        props['type']['pattern'] = "^{}$".format(endpoint)
 
         return schema
