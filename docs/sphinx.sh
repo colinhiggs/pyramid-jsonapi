a843d1796cffcd8b4aac44945147ec48b9894763
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-11-13 15:54:39 +0000

    Workaround https://github.com/Syntaf/travis-sphinx/issues/28

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -8,9 +8,9 @@ PATH=bin/:$PATH
 sphinx-apidoc -f -T -e -o docs/source/apidoc pyramid_jsonapi
 # Generate config docs from python method
 python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); print(s.sphinx_doc())' >docs/source/apidoc/settings.inc
-travis-sphinx build
+travis-sphinx build --source=docs/source
 # Get a pylint badge
-wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O target/doc/build/pylint-badge.svg
+wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O doc/build/pylint-badge.svg
 if [[ $TRAVIS_BRANCH == "master" && -n $TRAVIS_TAG ]]; then
   travis-sphinx deploy
 fi
