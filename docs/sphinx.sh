2aca4179ca69f90c4d266f5b6de7c5d1d0ea589a
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-11-15 12:22:46 +0000

    Args ordering is important.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -10,10 +10,10 @@ TARGET=docs/build
 sphinx-apidoc -f -T -e -o ${SOURCE}/apidoc pyramid_jsonapi
 # Generate config docs from python method
 python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); print(s.sphinx_doc())' >docs/source/apidoc/settings.inc
-travis-sphinx build --source=${SOURCE} --outdir=${TARGET}
+travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
 # Get a pylint badge
 wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
 if [[ $TRAVIS_BRANCH == "master" && -n $TRAVIS_TAG ]]; then
   echo "Deploying docs to gh-pages..."
-  travis-sphinx deploy --outdir=${TARGET}
+  travis-sphinx --outdir=${TARGET} deploy
 fi
