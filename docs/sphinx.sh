1a64f317432de54e7def3545288d01cb26fc69ed
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-11-15 13:16:46 +0000

    Alternative way to see if this is a tagged master branch.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -13,7 +13,8 @@ python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); print(s
 travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
 # Get a pylint badge
 wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
-if [[ $TRAVIS_BRANCH == "master" && -n $TRAVIS_TAG ]]; then
+# Build docs if this is master branch, and HEAD has a tag associated with it
+if [[ $TRAVIS_BRANCH == "master" ]] && git describe --exact-match HEAD; then
   echo "Deploying docs to gh-pages..."
   travis-sphinx --outdir=${TARGET} deploy
 fi
