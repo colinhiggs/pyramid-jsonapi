0dac333d1315bab4205ff03e688409bbc04290e0
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-11-21 16:10:20 +0000

    Only get new badge when deploying master docs.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -11,10 +11,10 @@ sphinx-apidoc -f -T -e -o ${SOURCE}/apidoc pyramid_jsonapi
 # Generate config docs from python method
 python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); print(s.sphinx_doc())' >docs/source/apidoc/settings.inc
 travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
-# Get a pylint badge
-wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
 # Build docs if this is master branch, and HEAD has a tag associated with it
 if [[ $TRAVIS_BRANCH == "master" ]] && git describe --exact-match HEAD; then
+  # Get a pylint badge
+  wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
   echo "Deploying docs to gh-pages..."
   travis-sphinx --outdir=${TARGET} deploy
 fi
