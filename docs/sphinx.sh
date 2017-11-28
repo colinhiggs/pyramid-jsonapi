6d58e381305140817fc98bbd7f0c3bec69f9b33f
Matthew Richardson <M.Richardson@ed.ac.uk>
2017-11-28 16:34:59 +0000

    Use github url. Add timeout to prevent stuck travis.

--- docs/sphinx.sh
+++ docs/sphinx.sh
@@ -12,7 +12,7 @@ sphinx-apidoc -f -T -e -o ${SOURCE}/apidoc pyramid_jsonapi
 python -c 'import pyramid_jsonapi.settings as pjs; s = pjs.Settings({}); print(s.sphinx_doc())' >docs/source/apidoc/settings.inc
 travis-sphinx --outdir=${TARGET} build --source=${SOURCE}
 # Get a pylint badge
-wget https://mperlet.de/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
+wget --tries=3 --timeout=20 https://mperlet.github.io/pybadge/badges/$(pylint pyramid_jsonapi |grep "rated at" |awk '{print $7}' |cut -f 1 -d '/').svg -O ${TARGET}/pylint-badge.svg
 # Build docs if this is master branch, and HEAD has a tag associated with it
 if [[ $TRAVIS_BRANCH == "master" ]] && git describe --exact-match HEAD; then
   echo "Deploying docs to gh-pages..."
