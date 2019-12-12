0265ddc0d2f11c5676cadc83d2b618947c4a40ab
Colin Higgs <colin.higgs@ed.ac.uk>
2019-12-12 16:43:45 +0000

    remember to add collection_get_loop.py

---
+++ pyramid_jsonapi/workflow/collection_get_loop.py
@@ -0,0 +1,48 @@
+import pyramid_jsonapi.jsonapi
+import pyramid_jsonapi.workflow as wf
+import sqlalchemy
+
+from pyramid.httpexceptions import (
+    HTTPBadRequest,
+)
+
+stages = (
+    'alter_query',
+    'alter_results',
+    'alter_related_query',
+    'alter_related_results',
+)
+
+def workflow(view, stages, data):
+    query = view.base_collection_query()
+    query = view.query_add_sorting(query)
+    query = view.query_add_filtering(query)
+    try:
+        count = query.count()
+    except sqlalchemy.exc.ProgrammingError:
+        raise HTTPInternalServerError(
+            'An error occurred querying the database. Server logs may have details.'
+        )
+    qinfo = view.collection_query_info(view.request)
+    query = query.offset(qinfo['page[offset]'])
+    limit = qinfo['page[limit]']
+    query = query.limit(limit)
+    query = wf.execute_stage(
+        view, stages, 'alter_query', query
+    )
+    try:
+        res_objs = [wf.ResultObject(view, o) for o in query.all()]
+    except sqlalchemy.exc.DataError as exc:
+        raise HTTPBadRequest(str(exc.orig))
+    results = wf.Results(
+        view,
+        objects=res_objs,
+        many=True,
+        is_top=True,
+        count=count,
+        limit=limit
+    )
+    results = wf.execute_stage(view, stages, 'alter_results', results)
+    for res in results.objects:
+        wf.fill_related(stages, res)
+    return results.serialise()
