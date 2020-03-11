9badaffceb3446d1479d9325e79855de1b34a7b2
Colin Higgs <colin.higgs@ed.ac.uk>
2020-03-11 23:57:19 +0000

    Move test out of tmp area

--- test_project/test_project/tests.py
+++ test_project/test_project/tests.py
@@ -159,21 +159,7 @@ class DBTestBase(unittest.TestCase):
 
 class TestTmp(DBTestBase):
     '''To isolate tests so they can be run individually during development.'''
-    def test_adjacancy_list(self):
-        top = self.test_app().get('/treenodes/1').json
-        top_1 = self.test_app().get('/treenodes/2').json
-        # top should have no parent.
-        self.assertIsNone(top['data']['relationships']['parent']['data'])
-        # top should have multiple children.
-        self.assertIsInstance(top['data']['relationships']['children']['data'], list)
-        # top_1 should have top as a parent.
-        self.assertEqual(
-            top_1['data']['relationships']['parent']['data'],
-            {'type': 'treenodes', 'id': '1'}
-        )
-        # top_1 should have 2 children.
-        self.assertIsInstance(top_1['data']['relationships']['children']['data'], list)
-        self.assertEqual(len(top_1['data']['relationships']['children']['data']), 2)
+
 
 class TestRelationships(DBTestBase):
     '''Test functioning of relationsips.
@@ -1137,6 +1123,24 @@ class TestRelationships(DBTestBase):
             headers={'Content-Type': 'application/vnd.api+json'},
             status=400
         )
+
+    def test_adjacancy_list(self):
+        '''Should correctly identify parent and children for TreeNode.
+        '''
+        top = self.test_app().get('/treenodes/1').json
+        top_1 = self.test_app().get('/treenodes/2').json
+        # top should have no parent.
+        self.assertIsNone(top['data']['relationships']['parent']['data'])
+        # top should have multiple children.
+        self.assertIsInstance(top['data']['relationships']['children']['data'], list)
+        # top_1 should have top as a parent.
+        self.assertEqual(
+            top_1['data']['relationships']['parent']['data'],
+            {'type': 'treenodes', 'id': '1'}
+        )
+        # top_1 should have 2 children.
+        self.assertIsInstance(top_1['data']['relationships']['children']['data'], list)
+        self.assertEqual(len(top_1['data']['relationships']['children']['data']), 2)
 
 
 class TestSpec(DBTestBase):
