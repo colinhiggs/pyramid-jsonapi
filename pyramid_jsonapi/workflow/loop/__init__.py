836ace8ddb6ac76b0c1c0c6f8d8a00ab51757918
Colin Higgs <colin.higgs@ed.ac.uk>
2021-06-18 09:00:32 +0100

    remove some unused functions

--- pyramid_jsonapi/workflow/loop/__init__.py
+++ pyramid_jsonapi/workflow/loop/__init__.py
@@ -94,53 +94,6 @@ def fill_result_object_related(res_obj, stages):
             )
 
 
-def fill_related(stages, obj, include_path=None):
-    view = obj.view
-    if include_path is None:
-        include_path = []
-    for rel_name, rel in view.relationships.items():
-        rel_include_path = include_path + [rel_name]
-        is_included = False
-        if '.'.join(rel_include_path) in view.requested_include_names():
-            is_included = True
-        if rel_name not in view.requested_relationships and not is_included:
-            continue
-        if not view.mapped_info_from_name(rel_name).get('visible', True):
-            continue
-
-        rel_view = view.view_instance(rel.tgt_class)
-        query = view.related_query(obj.obj_id, rel, full_object=is_included)
-        many = rel.direction is ONETOMANY or rel.direction is MANYTOMANY
-        if many:
-            count = query.count()
-            limit = view.related_limit(rel)
-            query = query.limit(limit)
-        query = wf.execute_stage(
-            view, stages, 'alter_related_query', query
-        )
-
-        try:
-            rel_results_objs = [wf.ResultObject(rel_view, o) for o in query.all()]
-        except sqlalchemy.exc.DataError as exc:
-            raise HTTPBadRequest(str(exc.orig))
-        rel_results = wf.Results(
-            rel_view,
-            objects=rel_results_objs,
-            many=many,
-            is_included=is_included
-        )
-        rel_results = wf.execute_stage(
-            view, stages, 'alter_related_results', rel_results
-        )
-        if is_included:
-            for rel_obj in rel_results.objects:
-                fill_related(stages, rel_obj, include_path=rel_include_path)
-        obj.related[rel_name] = rel_results
-        if many:
-            obj.related[rel_name].count = count
-            obj.related[rel_name].limit = limit
-
-
 def get_alter_handler(view, obj, pdata, stage_name='alter_result'):
     reason = "Permission denied."
     predicate = view.permission_filter('get', stage_name)
@@ -180,15 +133,6 @@ def permission_handler(endpoint_name, stage_name):
             pass
         return results
 
-    def get_alter_direct_results_handler(view, results, pdata):
-        results = apply_results_filter(results, 'alter_direct_results', view)
-        if not results.many and results.rejected_objects:
-            raise HTTPNotFound('No item {} in {}'.format(view.obj_id, view.collection_name))
-        return results
-
-    def get_alter_related_results_handler(view, results, pdata):
-        return apply_results_filter(results, 'alter_related_results', view)
-
     def get_alter_results_handler(view, results, pdata):
         apply_results_filter(results, 'alter_results', view)
         if not results.many and results.rejected_objects:
@@ -200,8 +144,6 @@ def permission_handler(endpoint_name, stage_name):
 
     handlers = {
         'get': {
-            'alter_direct_results': get_alter_direct_results_handler,
-            'alter_related_results': get_alter_related_results_handler,
             'alter_result': get_alter_handler,
             'alter_related_result': partial(get_alter_handler, stage_name='alter_related_result'),
             'alter_results': get_alter_results_handler,
